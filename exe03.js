ae5fe3285e112787fd612411388fe799de4a2a88
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-07-31 23:51:41 -0300

    variados métodos de declarar variáveis

---
+++ exe03.js
@@ -0,0 +1,14 @@
+//Aqui alguns métodos de como posso declarar uma variável de maneiras diferentes
+ let nome="Bruno"
+
+ {
+  let siglacurso="js"
+  console.log(siglacuro)//Essa variável só é acessível dentro da função.
+ }
+
+ const curso=()=>{
+  let curso="javascript"
+  console.log(curso)//Essa variável só é acessível dentro da função oculpa um pouco de memoria.
+ }
+
+ curso()
