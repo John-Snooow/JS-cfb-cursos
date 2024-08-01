34b1c3c6642cb7ff88ae946eb0eee354a71e11d3
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-07-31 22:31:14 -0300

    operadores relacionais

---
+++ aula003-operadores-relacionais/exe01.js
@@ -0,0 +1,23 @@
+/*
+operadores relacionais são os que vão determinar se uma operação vai ser verdadeira ou falsa
+
+  >  <  >=  <=  ==  !=
+*/
+
+let num1= 10
+let num2= 20
+let num3= 10
+
+console.log(num1>num2)// aqui estou dizendo que o num1 é maior que num2 tornando assim uma operação falsa
+
+//são operaçõies de conparações
+
+console.log(num1>=num3) //aqui num1 não é maior que num3 mais eles são iguais tornando assim uma operação verdadeira
+
+console.log(num1==num3) //verdadeiro
+
+console.log(!(num1==num3))//aqui eu estou fazendo uma inversão tornando aqui a firmaão de negação ou falso
+
+console.log(num1!=num3)// aqui seria falso porque num não é diferente de num3 como esta afirmando 
+
+console.log(!(num1!=num3))// aqui outra vez eu fiz uma inversão dizendo que num1não é diferente de num3 tornando aqui uma afirmação verdadeira
