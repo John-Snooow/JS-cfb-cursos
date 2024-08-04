a86d45f9a79eec312bdae7a649035386fde63050
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-08-03 23:28:29 -0300

    operando os bits

---
+++ aula005-operadores-bitwise/exe.01.js
@@ -0,0 +1,10 @@
+//operando os bits dos elementos aula9 CBFcursos
+
+let n1= 10
+let n2 = 11
+
+//let res = n1 & n2 //aqui vai dar resultado 10 
+//let res = n1 | n2  //aqui vai dar resultado 11
+let res = n1 ^ n2   //aqui vai dar resultado 1
+
+console.log(res) 
---
+++ aula005-operadores-bitwise/exe02.js
@@ -0,0 +1,7 @@
+//deslocamento de bit
+
+let n1= 10
+
+let res = n1 << 2 // aqui estou deslocando dois bits para esquerda ficando asssim com resultado de 40, 
+
+console.log(res) 
