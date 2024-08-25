50fd6d51129aa024becf513f165a12b1c49b5e0d
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-08-24 23:42:56 -0300

    fazendo alguns comentários

--- aula014-loop-while/exe03.js
+++ aula014-loop-while/exe03.js
@@ -1,13 +1,13 @@
-//fatorial de 5
+// Calcula o fatorial de 5
 // 5! = 5*4*3*2*1 = 120
 
-let n5
-let fat=1
+let n = 5 
+let fat = 1
 
-while(n>=1){
-  fat*=n
-  //fat=fat*n
-  n--
+while(n >= 1){ // Enquanto n for maior ou igual a 1, execute o bloco de código
+  fat *= n // Multiplica o resultado atual do fatorial pelo valor de n
+  //fat = fat * n // Essa linha é equivalente à anterior, mas usando a sintaxe de atribuição
+  n-- // Decrementa o valor de n em 1
 }
 
-console.log(fat) // 120
+console.log(fat) // Imprime o resultado do fatorial, que é 120
