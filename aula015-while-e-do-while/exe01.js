a10405dec6533e6c62c08f4cbd746a78d046747a
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-08-26 23:31:49 -0300

    while e do-while

---
+++ aula015-while-e-do-while/exe01.js
@@ -0,0 +1,21 @@
+let n= 10
+/* 
+while (n<10){
+    console.log("CFB curso")
+    n++
+}
+console.log("fim do programa")
+*/
+
+do{
+    console.log("CFB curso")
+    n++
+}while (n<10)
+console.log("fim do programa")
+
+
+/*
+Em resumo, o laço do...while executa o bloco de código dentro do do uma vez, independentemente da condição do while, e depois verifica a condição. Se a condição for falsa, o laço termina e o programa continua executando o código após o laço.
+
+No caso deste código, como a condição n < 10 é falsa desde o início (pois n é inicializado com 10), o laço do...while só executa o bloco de código dentro do do uma vez e depois termina.
+*/
