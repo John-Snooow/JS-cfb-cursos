2e5c83829b6f01b834704579e3d6b0482b6e66c8
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-09-18 22:32:14 -0300

    funções

---
+++ aula017-funções/exe02.js
@@ -0,0 +1,8 @@
+function mudarTexto(){
+  let d1=document.getElementById("d1")//Seleciona o elemento HTML com o id "d1" e armazena em uma variável chamada d1.
+  let d2=document.getElementById("d2")//Altera o conteúdo do elemento d1 para "CFB cursos".
+  let d3=document.getElementById("d3")
+  d1.innerHTML="John"
+  d2.innerHTML="Frank"
+  d3.innerHTML="CFB cursos"
+}
